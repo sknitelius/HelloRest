9dca9dd541c7f75276fa468f6d8fefbff3ac46c2
sknitelius <stephan@knitelius.com>
2017-03-09 21:54:37 +0100

    Added Smoke tests.

--- src/test/java/com/knitelius/hellorest/rest/HelloRestControllerTest.java
+++ src/test/java/com/knitelius/hellorest/rest/HelloRestControllerTest.java
@@ -1,23 +1,27 @@
 package com.knitelius.hellorest.rest;
 
+import static org.junit.Assert.*;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.knitelius.hellorest.rest.client.HelloRestControllerClient;
 
 public class HelloRestControllerTest {
 
 	private static String url;
-	
+
 	@BeforeClass
 	public static void beforeClass() {
 		url = System.getProperty("url") != null ? System.getProperty("url") : "http://localhost:9080/hellorest";
-		System.out.println(String.format("url: %s", url)); 
+		System.out.println(String.format("url: %s", url));
 	}
-	
+
 	@Test
 	public void test() {
-		//TODO
+		HelloRestControllerClient client = new HelloRestControllerClient(url);
+		String returnValue = client.invokeMessages();
+		assertEquals("Hello Rest!", returnValue);
 	}
 
 }
