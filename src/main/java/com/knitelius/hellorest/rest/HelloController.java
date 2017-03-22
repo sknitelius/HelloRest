b93abdc5ff67fccfb266ff21342c7bfe388841a5
sknitelius <stephan@knitelius.com>
2017-03-21 23:05:40 -0700

    Preperation for demonstration.

--- src/main/java/com/knitelius/hellorest/rest/HelloController.java
+++ src/main/java/com/knitelius/hellorest/rest/HelloController.java
@@ -25,15 +25,14 @@ package com.knitelius.hellorest.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
 
 
 @Path("hello")
 public class HelloController {
     
 	@GET
-	public String message(@QueryParam("name") String name) {
-		return String.format("Hello %s!", name);
+	public String message() {
+		return "Hello InterConnect";
 	}
 	
 }
