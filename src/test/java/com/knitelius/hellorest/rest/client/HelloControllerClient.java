3b71d1c36dbd173f36c018745409adda1da650f6
sknitelius <stephan@knitelius.com>
2017-03-21 18:10:59 -0700

    Preparing for presentation.

--- src/test/java/com/knitelius/hellorest/rest/client/HelloControllerClient.java
+++ src/test/java/com/knitelius/hellorest/rest/client/HelloControllerClient.java
@@ -6,13 +6,12 @@ import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 import java.lang.String;
-import javax.ws.rs.core.MediaType;
 
-public class HelloRestControllerClient {
+public class HelloControllerClient {
 
 	final String resourceUrl;
 	
-	public HelloRestControllerClient(String resourceUrl) {
+	public HelloControllerClient(String resourceUrl) {
 		this.resourceUrl = resourceUrl;
 	}
 	
@@ -22,14 +21,6 @@ public class HelloRestControllerClient {
 		target = target.queryParam("name", s);
 		
 		Invocation.Builder builder = target.request();
-		Response response = builder.get();
-		return response.readEntity(String.class);
-	}
-
-	public String invokeMessages(){
-		Client client = ClientBuilder.newBuilder().build();
-		WebTarget target = client.target(resourceUrl + "/hello");
-		Invocation.Builder builder = target.request("text/plain");
 		Response response = builder.get();
 		return response.readEntity(String.class);
 	}
