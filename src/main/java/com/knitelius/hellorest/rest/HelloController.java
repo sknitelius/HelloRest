ae1c324b21d8a96fb4145aefbe09b500fe744c7d
sknitelius <stephan@knitelius.com>
2017-03-21 21:23:18 -0700

    clean up.

--- src/main/java/com/knitelius/hellorest/rest/HelloController.java
+++ src/main/java/com/knitelius/hellorest/rest/HelloController.java
@@ -25,9 +25,7 @@ package com.knitelius.hellorest.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
 
 
 @Path("hello")
